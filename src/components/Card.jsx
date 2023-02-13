95ae860afa8f804d379ccf29acb5633cdc796415
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-02-13 15:32:23 +0200

    added elements to the project

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 
+// imports
+import NFTImage from "../assets/nft-image.png";
+
 const Card = () => {
   return (
-    <div>
-      <div>Hey</div>
-    </div>
+    <>
+      <div className="card__wrapper">
+        <div className="card__top">
+          <img
+            src={NFTImage}
+            alt="NFT Card Image"
+            className="card__top_image"
+          />
+        </div>
+        <div className="card__bottom">
+          <p className="card__bottom_title">#3429</p>
+          <p className="card__bottom_description">
+            Our Equilibrium collection promotes balance and calm.
+          </p>
+
+          <div className="card__bottom_price">
+            <p className="card__bottom_price_title">0.041 ETH</p>
+            <p className="card__bottom_price_time">3 days left</p>
+          </div>
+          <div className="card__bottom_user">
+            <div className="card__bottom_user_img"></div>
+            <p className="card__bottom_user_name">Creation of Jules Wyvern</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
