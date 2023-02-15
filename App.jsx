83536541f9489e4b573a43549ad4e90bb4a35ffe
Luca Liebenberg <liebenbergluca@gmail.com>
2023-02-15 20:47:03 +0200

    Only single NFT card in App

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+// imports
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+};
+
+export default App;
---
+++ components/Card.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+// imports
+import NFTImage from "../assets/nft-image.png";
+import Avatar from "../assets/avatar-image.png";
+
+const Card = () => {
+  return (
+    <>
+      <div className="card__wrapper">
+        <div className="card__top">
+          <div className="card__top_wrapper">
+            <img
+              src={NFTImage}
+              alt="NFT Card Image"
+              className="card__top_image"
+            />
+            <div className="overlay"></div>
+          </div>
+        </div>
+        <div className="card__bottom">
+          <p className="card__bottom_title">
+            <span class="extra">Equilibrium</span> #3429
+          </p>
+          <p className="card__bottom_description">
+            Our Equilibrium collection promotes balance and calm.
+          </p>
+
+          <div className="card__bottom_price">
+            <p className="card__bottom_price_title">0.041 ETH</p>
+            <p className="card__bottom_price_time">3 days left</p>
+          </div>
+          <div className="card__bottom_line"></div>
+
+          <div className="card__bottom_user">
+            <div className="card__bottom_user_img">
+              <img
+                src={Avatar}
+                alt="Avatar Image"
+                className="card__bottom_user_image"
+              />
+            </div>
+            <p className="card__bottom_user_name">
+              Creation of{" "}
+              <span className="card__bottom_user_name_white">Jules Wyvern</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
